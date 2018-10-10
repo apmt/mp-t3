3de6e8b99735af83f51570317f55c81014565f8b
apmt <43222877+apmt@users.noreply.github.com>
2018-10-10 18:27:01 -0300

    Add files via upload

--- testa_linhas.cpp
+++ testa_linhas.cpp
@@ -1,19 +1,16 @@
 // Copyright [2018] <Ana Paula Martins Tarchetti>"  [legal/copyright]
 #include <gtest/gtest.h>
 #include "linhas.cpp"
-#define sim 1
-#define nao 0
-#define erro 99999
 TEST(LinhasTest, exemplo_comum) {
-    FILE *arquivo = fopen("exemplo_1.cpp", "r");
+    FILE *arquivo_1 = fopen("exemplo_1.cpp", "r");
     ASSERT_EQ(5, quantidade_linhas(arquivo));
-    fclose(arquivo);
+    fclose(arquivo_1);
 }
 
 TEST(LinhasTest, exemplo_linhas_vazias) {
-    FILE *arquivo = fopen("exemplo_2.cpp", "r");
+    FILE *arquivo_2 = fopen("exemplo_2.cpp", "r");
     ASSERT_EQ(5, quantidade_linhas(arquivo));
-    fclose(arquivo);
+    fclose(arquivo_2);
 }
 
 int main(int argc, char **argv) {
