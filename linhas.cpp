37cfceb7b767f952505fefea1c51ce1dc6c5be96
apmt <43222877+apmt@users.noreply.github.com>
2018-10-10 18:02:05 -0300

    Update linhas.cpp

--- linhas.cpp
+++ linhas.cpp
@@ -1,7 +1,24 @@
 // Copyright [2018] <Ana Paula Martins Tarchetti>"  [legal/copyright]
 #include "../include/linhas.h"
 #include<stdlib.h>
+#define nao 0
+#define sim 1
 int quantidade_linhas(FILE *arquivo){
-    int aux;
-    return aux;
+int quantidade=0;
+char caractere;
+int only_spaces;
+only_spaces=sim;
+do
+{
+    fscanf(arquivo, "%c", &caractere);
+    if(feof(arquivo))
+    {
+    break;
+    }
+    if(caractere == '\n')
+    {
+    quantidade++;
+    }
+}while(1);
+return quantidade;
 }
