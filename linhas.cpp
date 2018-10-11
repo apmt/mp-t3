80377ebe172175f50c76a0318136679a8fe4d46e
apmt <43222877+apmt@users.noreply.github.com>
2018-10-10 22:41:17 -0300

    Add files via upload

--- linhas.cpp
+++ linhas.cpp
@@ -6,20 +6,56 @@
 int quantidade_linhas(FILE *arquivo){
 int quantidade=0;
 char caractere;
-int only_spaces;
+int only_spaces, fim_comentario, pulou_linha;
 only_spaces=sim;
 do
 {
-    fscanf(arquivo, "%c", &caractere);
-    if(feof(arquivo))
+    if(feof(arquivo)!=0)
     {
     break;
     }
+    fscanf(arquivo, "%c", &caractere);
     if(caractere == '/'){
         fscanf(arquivo, "%c", &caractere);
         if(caractere == '/' && only_spaces == sim){
-            while(caractere != '\n' && feof(arquivo)== 0){
-                fscanf(arquivo, "%c", &caractere);        
+            while(caractere != '\n' && feof(arquivo) == 0){
+                fscanf(arquivo, "%c", &caractere);            
+            }
+        }
+        if(caractere == '*'){
+            if(only_spaces == sim){
+                fim_comentario = nao;
+                do{
+                    fscanf(arquivo, "%c", &caractere);
+                        if(caractere == '*'){
+                            fscanf(arquivo, "%c", &caractere);
+                            if(caractere == '/'){
+                                fim_comentario=sim;
+                            }
+                        }
+                }while(feof(arquivo)== 0 && fim_comentario == nao);
+                fscanf(arquivo, "%c", &caractere);
+            }
+            if(only_spaces == nao){
+                pulou_linha = nao;
+                fim_comentario = nao;
+                do{
+                    fscanf(arquivo, "%c", &caractere);
+                        if(caractere == '\n'){
+                                pulou_linha = sim;
+                        }
+                        if(caractere == '*'){
+                            fscanf(arquivo, "%c", &caractere);
+                            if(caractere == '/'){
+                                fim_comentario=sim;
+                            }
+                        }
+                }while(feof(arquivo)== 0 && fim_comentario == nao);
+                fscanf(arquivo, "%c", &caractere);
+                if(pulou_linha == sim){
+                    quantidade++;
+                    only_spaces = sim;                
+                }
             }
         }
     }
--- testa_linhas.cpp
+++ testa_linhas.cpp
@@ -3,20 +3,34 @@
 #include "linhas.cpp"
 TEST(LinhasTest, exemplo_comum) {
     FILE *arquivo = fopen("exemplo_1.cpp", "r");
+    if(arquivo!=NULL){
     ASSERT_EQ(5, quantidade_linhas(arquivo));
     fclose(arquivo);
+    }
 }
 
 TEST(LinhasTest, exemplo_linhas_vazias) {
     FILE *arquivo = fopen("exemplo_2.cpp", "r");
+    if(arquivo!=NULL){
     ASSERT_EQ(5, quantidade_linhas(arquivo));
     fclose(arquivo);
+    }
 }
 
-TEST(LinhasTest, exemplo_comentarios_barrabarra) {
+TEST(LinhasTest, exemplo_comentarios_barra_barra) {
     FILE *arquivo = fopen("exemplo_3.cpp", "r");
+    if(arquivo!=NULL){
     ASSERT_EQ(5, quantidade_linhas(arquivo));
     fclose(arquivo);
+    }
+}
+
+TEST(LinhasTest, exemplo_comentarios_barra_asterisco) {
+    FILE *arquivo = fopen("exemplo_4.cpp", "r");
+    if(arquivo!=NULL){
+    ASSERT_EQ(5, quantidade_linhas(arquivo));
+    fclose(arquivo);
+    }
 }
 
 
