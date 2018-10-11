d788b82f504ad40179660822979912c30942cf69
apmt <43222877+apmt@users.noreply.github.com>
2018-10-10 22:42:17 -0300

    Add files via upload

---
+++ exemplo_3.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    //coment1 coment2
+//coment3
+	printf("Hello World\n");//coment4
+	return 0;
+}
+//coment5 coment6
+//
---
+++ exemplo_4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/***/	/**/
+/*coment*/
+/*coment
+*/
+int main(){
+    /* */printf("Hello World\n");/*
+*/    return 0;/* */
+}/*coment
+*/
+	/*coment*/
+/*
+coment
+*/
